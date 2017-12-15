638c76886475045bd24e79505ed91f51db2a84c7
NicolasSporrer <nsporrer@ensc.fr>
2017-12-15 17:53:12 +0100

    Commit initial

---
+++ ClassLibrary1/Repository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate;
+using NHibernate.Cfg;
+
+namespace DAL
+{
+        public abstract class Repository
+        {
+            private static ISessionFactory sessionFactory;
+            private static ISession session;
+
+            private static ISessionFactory SessionFactory
+            {
+                get
+                {
+                    // Crée l'objet SessionFactory au premier accès (lazy loading)
+                    if (sessionFactory == null)
+                    {
+                        sessionFactory = new Configuration().Configure().BuildSessionFactory();
+                    }
+                    return sessionFactory;
+                }
+            }
+
+            protected static ISession Session
+            {
+                get
+                {
+                    // Crée l'objet Session au premier accès (lazy loading)
+                    if (session == null)
+                    {
+                        session = SessionFactory.OpenSession();
+                    }
+                    return session;
+                }
+            }
+        }
+}
---
+++ Domain/Account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Account
+    {
+        string _password;
+        string _login;
+
+
+
+        public void verificateurMdp(string password)
+        {
+
+        }
+
+
+
+    }
+}
